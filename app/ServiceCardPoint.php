d682afe75cc002bd6b70375e3f9e0ef1714eba5c
mohsinkhan89 <mk8979494@gmail.com>
2026-01-28 18:01:15 +0500

    changes done

--- app/ServiceCardPoint.php
+++ app/ServiceCardPoint.php
@@ -3,9 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Service;
 
 class ServiceCardPoint extends Model
 {
-    
+
+    public function cardDetail()
+    {
+        return $this->belongsTo(ServiceCardDetail::class, 'service_card_detail_id');
+    }
+    // public $additional_attributes = ['title_with_door'];
+
+    // public function getTitleWithDoorAttribute()
+    // {
+    //     return $this->title . ' - ' . $this->door_detail->title;
+    // }
+
+    // public function door_detail(){
+    //     return $this->belongsTo(Service::class , 'door_id');
+    // }
+    // public function doordetail_color(){
+    //     return $this->hasMany(Service::class , "door_to_details_to_designs_id");
+    // }
 }
